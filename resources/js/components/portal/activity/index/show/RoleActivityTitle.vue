459fa1b6743405123ede05214d9980972fbebeea
Toby Twigger <tt15951@my.bristol.ac.uk>
2020-02-05 00:50:09 +0000

    Update role activity titleg

--- resources/js/components/portal/activity/index/show/RoleActivityTitle.vue
+++ resources/js/components/portal/activity/index/show/RoleActivityTitle.vue
@@ -13,7 +13,9 @@
 
         data() {
             return {
-                role: null
+                role: null,
+                position: null,
+                group: null
             }
         },
 
@@ -24,8 +26,22 @@
         methods: {
             loadRole() {
                 this.$control.role().get(this.roleId)
-                    .then(response => this.role = response.data)
+                    .then(response => {
+                        this.role = response.data;
+                        this.loadGroup();
+                        this.loadPosition();
+                    })
                     .catch(error => this.$notify.alert('Could not load role ' + this.roleId + ': ' + error.message));
+            },
+            loadGroup() {
+                this.$control.group().get(this.role.group_id)
+                    .then(response => this.group = response.data)
+                    .catch(error => this.$notify.alert('Could not load group ' + this.role.group_id + ': ' + error.message));
+            },
+            loadPosition() {
+                this.$control.position().get(this.role.position_id)
+                    .then(response => this.position = response.data)
+                    .catch(error => this.$notify.alert('Could not load position ' + this.position.position_id + ': ' + error.message));
             }
         },
 
@@ -34,7 +50,9 @@
                 if(this.role === null) {
                     return 'Loading role information...';
                 }
-                return  'Activities for your position of ' + this.role.position.data.name + ' to ' + this.role.group.data.name;
+                let roleName = (this.role.data.role_name || (this.position === null ? '...loading...' : this.position.data.name ) );
+                let groupName = (this.group === null ? '...loading...' : this.group.data.name );
+                return 'Activities for your position of ' + roleName + ' to ' + groupName;
             },
 
         }
