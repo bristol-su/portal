b366f3fd00fa48739463deca06b1760a2abf1c83
Toby Twigger <tobytwigger1@gmail.com>
2020-04-01 09:07:01 +0100

    Update analytics

---
+++ resources/views/partials/analytics.blade.php
@@ -0,0 +1,14 @@
+@if(config('app.analytics.enabled', false))
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{config('app.analytics.UA')}}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', '{{config('app.analytics.UA')}}');
+    </script>
+@endif
