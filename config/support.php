16047e5bb5bc6534734a4d8d74caa77fdeb70733
Toby Twigger <tobytwigger1@gmail.com>
2020-04-03 12:21:00 +0100

    Update support

--- config/support.php
+++ config/support.php
@@ -3,5 +3,16 @@
 return [
     'analytics' => [
         'enabled' => env('ANALYTICS_ENABLED', false)
+    ],
+    'revision' => [
+        'cleanup' => [
+            'enabled' => env('REVISION_CLEANUP_ENABLED', true),
+            'limit' => env('REVISION_CLEANUP_LIMIT', 10000)
+        ]
+    ],
+    'caching' => [
+        'filters' => [
+            'enabled' => env('FILTER_CACHING_ENABLED', true)
+        ]
     ]
 ];
