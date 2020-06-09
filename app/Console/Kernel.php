b4f062ad391222d8b7fd0d51ef93d03d4167f77b
Toby Twigger <tobytwigger1@gmail.com>
2020-06-09 10:31:55 +0100

    Only run commands in production

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,7 +6,6 @@ use App\Console\Commands\CreateMissingActivityInstancesForAllActivities;
 use App\Console\Commands\RunUnionCloudCommands;
 use App\Console\Commands\UpdateProgress;
 use BristolSU\Support\Filters\Commands\CacheFilters;
-use BristolSU\Support\Filters\Jobs\CacheFilter;
 use BristolSU\Support\ModuleInstance\Contracts\Scheduler\CommandStore;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -30,18 +29,20 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(CacheFilters::class)->hourly()->runInBackground()->when(function() {
-            return config('support.caching.filters.enabled');
-        });
-        $schedule->command(UpdateProgress::class)->hourly()->runInBackground();
-        $schedule->command(RunUnionCloudCommands::class)->everyMinute()->runInBackground();
-        $schedule->command(CreateMissingActivityInstancesForAllActivities::class)->daily()->runInBackground();
-        $schedule->command('control:export role --exporter=committee-contact-sheet')->everyThirtyMinutes()->runInBackground();
-        $schedule->command('control:export role --exporter=committee-contact-sheet-old')->daily()->runInBackground();
+        if(app()->environment('production')) {
+            $schedule->command(CacheFilters::class)->hourly()->runInBackground()->when(function() {
+                return config('support.caching.filters.enabled');
+            });
+            $schedule->command(UpdateProgress::class)->hourly()->runInBackground();
+            $schedule->command(RunUnionCloudCommands::class)->everyMinute()->runInBackground();
+            $schedule->command(CreateMissingActivityInstancesForAllActivities::class)->daily()->runInBackground();
+            $schedule->command('control:export role --exporter=committee-contact-sheet')->everyThirtyMinutes()->runInBackground();
+            $schedule->command('control:export role --exporter=committee-contact-sheet-old')->daily()->runInBackground();
 
-        foreach (app(CommandStore::class)->all() as $alias => $commands) {
-            foreach ($commands as $command => $cron) {
-                $schedule->command($command)->cron($cron);
+            foreach (app(CommandStore::class)->all() as $alias => $commands) {
+                foreach ($commands as $command => $cron) {
+                    $schedule->command($command)->cron($cron);
+                }
             }
         }
     }
