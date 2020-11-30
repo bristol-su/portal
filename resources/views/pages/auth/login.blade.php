dbf6ac0a9f5fcf59e2630e2d89f0e5ae71e7a746
Toby Twigger <tobytwigger1@gmail.com>
2020-11-30 22:06:17 +0000

    Tempalte changes

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -4,20 +4,29 @@
 
 @section('app-content')
 
-    <x-theme-container>
-        <x-theme-row
-            justify="center"
-            align="center"
-            class="fill-height">
-            <x-theme-col cols="12" sm="8" md="4">
-                ABC123
-            </x-theme-col>
-        </x-theme-row>
+        <x-theme-alert variant="danger" :dismissible="true">
+            Uho, it looks like something went wrong! Give it another go :)
+        </x-theme-alert>
+        <x-theme-alert variant="success" :dismissible="true">
+            Or maybe it went right!
+        </x-theme-alert>
+        <x-theme-alert variant="warning" :dismissible="true">
+            Let's settle with being careful
+        </x-theme-alert>
+        <x-theme-alert variant="info" :dismissible="false">
+            And keep us <a href="#">informed</a>
+        </x-theme-alert>
 
-        <x-theme-button type="success">
+        <x-theme-button variant="info">
             Test
         </x-theme-button>
-    </x-theme-container>
+
+        <x-theme-spinner variant="info" size="lg" alt="Loading...">
+
+        </x-theme-spinner>
+        <x-theme-spinner variant="info" alt="Loading...">
+
+        </x-theme-spinner>
     {{--            <p-form--}}
     {{--                v-slot="{ invalid }"--}}
     {{--                :action="$tools.routes.named.path('login')"--}}
