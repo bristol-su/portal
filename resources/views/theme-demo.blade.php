401dbc5853f62381a26982abc6a3c7737312a282
Toby Twigger <tobytwigger1@gmail.com>
2020-12-10 00:07:47 +0000

    Update theme demo

--- resources/views/theme-demo.blade.php
+++ resources/views/theme-demo.blade.php
@@ -97,49 +97,55 @@
 
 <hr/>
 
-<x-theme-checkbox
-    id="some-checkbox"
-    name="my-checkbox"
-    label="This is my checkbox"
-    help="First checkbox!"
-    sr-label="A checkbox for the demo page. This can only be seen by screen readers."
-    :errors="[]"
-    :disabled="true"
-    :validated="false"
-    value="test1"
-    :checked="true">
+<x-theme-fieldset legend="Checkboxes!!">
 
-</x-theme-checkbox>
+    <x-theme-checkbox
+        id="some-checkbox"
+        name="my-checkbox"
+        label="This is my checkbox"
+        help="First checkbox!"
+        sr-label="A checkbox for the demo page. This can only be seen by screen readers."
+        :errors="[]"
+        :disabled="true"
+        :validated="false"
+        value="test1"
+        :checked="true">
 
-<x-theme-checkbox
-    id="some-checkbox-2"
-    name="my-checkbox-2"
-    label="This is my checkbox 2"
-    help="Second checkbox!"
-    sr-label="Another checkbox for the demo page. This can only be seen by screen readers."
-    :errors="['First error', 'Maybe a second error too']"
-    :validated="true"
-    value="test2"
-    :required="true"
-    :checked="false">
+    </x-theme-checkbox>
 
-</x-theme-checkbox>
+    <x-theme-checkbox
+        id="some-checkbox-2"
+        name="my-checkbox-2"
+        label="This is my checkbox 2"
+        help="Second checkbox!"
+        sr-label="Another checkbox for the demo page. This can only be seen by screen readers."
+        :errors="['First error', 'Maybe a second error too']"
+        :validated="true"
+        value="test2"
+        :required="true"
+        :checked="false">
 
-<x-theme-checkbox
-    id="some-checkbox-3"
-    name="my-checkbox-3"
-    label="This is my checkbox 3"
-    help="Third checkbox!"
-    sr-label="A third checkbox for the demo page. This can only be seen by screen readers."
-    :errors="[]"
-    :validated="true"
-    value="test3"
-    :required="false"
-    :checked="true">
-</x-theme-checkbox>
+    </x-theme-checkbox>
+
+    <x-theme-checkbox
+        id="some-checkbox-3"
+        name="my-checkbox-3"
+        label="This is my checkbox 3"
+        help="Third checkbox!"
+        sr-label="A third checkbox for the demo page. This can only be seen by screen readers."
+        :errors="[]"
+        :validated="true"
+        value="test3"
+        :required="false"
+        :checked="true">
+    </x-theme-checkbox>
+</x-theme-fieldset>
 
 
 <hr/>
+
+<x-theme-fieldset legend="Select a radio">
+
 
 <x-theme-radio
     id="some-radio"
@@ -180,10 +186,171 @@
     :required="false"
     :checked="true">
 </x-theme-radio>
+</x-theme-fieldset>
 
+<hr/>
 
+<x-theme-text
+    id="some-text"
+    name="my-text"
+    label="This is my text input"
+    help="Can't type anything in, it's a disabled element!"
+    sr-label="A text for the demo page. This can only be seen by screen readers."
+    :errors="[]"
+    :disabled="true"
+    :validated="false"
+    :required="true"
+    value="The default input"
+>
 
+</x-theme-text>
 
+<x-theme-text
+    id="some-other-text"
+    name="my-text-2"
+    label="A text with errors"
+    help="This text has errors for validation"
+    sr-label="A text with errors for the demo page. This can only be seen by screen readers."
+    :errors="['This is the first error', 'And this is the second!']"
+    :validated="true"
+>
+
+</x-theme-text>
+
+<x-theme-text
+    id="some-other-text-3"
+    name="my-text-3"
+    label="This is a valid text"
+    help="This text has no errors, but has been validated"
+    sr-label="Something or other here."
+    :errors="[]"
+    :validated="true"
+>
+</x-theme-text>
+
+<hr/>
+
+<x-theme-email
+    id="some-email"
+    name="my-email"
+    label="This is my email input"
+    help="Can't type anything in, it's a disabled element!"
+    sr-label="A email for the demo page. This can only be seen by screen readers."
+    :errors="[]"
+    :disabled="true"
+    :validated="false"
+    :required="true"
+    value="The default input"
+>
+
+</x-theme-email>
+
+<x-theme-email
+    id="some-other-email"
+    name="my-email-2"
+    label="A email with errors"
+    help="This email has errors for validation"
+    sr-label="A email with errors for the demo page. This can only be seen by screen readers."
+    :errors="['This is the first error', 'And this is the second!']"
+    :validated="true"
+>
+
+</x-theme-email>
+
+<x-theme-email
+    id="some-other-email-3"
+    name="my-email-3"
+    label="This is a valid email"
+    help="This email has no errors, but has been validated"
+    sr-label="Something or other here."
+    :errors="[]"
+    :validated="true"
+>
+
+</x-theme-email>
+
+<hr/>
+
+<x-theme-password
+    id="some-password"
+    name="my-password"
+    label="This is my password input"
+    help="Can't type anything in, it's a disabled element!"
+    sr-label="A password for the demo page. This can only be seen by screen readers."
+    :errors="[]"
+    :disabled="true"
+    :validated="false"
+    :required="true"
+    value="The default input"
+>
+
+</x-theme-password>
+
+<x-theme-password
+    id="some-other-password"
+    name="my-password-2"
+    label="A password with errors"
+    help="This password has errors for validation"
+    sr-label="A password with errors for the demo page. This can only be seen by screen readers."
+    :errors="['This is the first error', 'And this is the second!']"
+    :validated="true"
+>
+
+</x-theme-password>
+
+<x-theme-password
+    id="some-other-password-3"
+    name="my-password-3"
+    label="This is a valid password"
+    help="This password has no errors, but has been validated"
+    sr-label="Something or other here."
+    :errors="[]"
+    :validated="true"
+>
+
+</x-theme-password>
+
+<hr/>
+
+@foreach([[true,true], [false,true], [true,false], [false,false]] as $vals)
+    <x-theme-list-group
+        :bordered="$vals[0]"
+        :horizontal="$vals[1]">
+
+        <x-theme-list-item
+            :active="true"
+            :disabled="false"
+            href="/test"
+            variant="info">
+            Item 1
+        </x-theme-list-item>
+
+        <x-theme-list-item
+            :active="false"
+            :disabled="false"
+            href="/test2"
+            variant="warning">
+            Item 2
+        </x-theme-list-item>
+
+        <x-theme-list-item
+            :active="false"
+            :disabled="true"
+            variant="danger">
+            Item 3
+        </x-theme-list-item>
+
+        <x-theme-list-item
+            :active="false"
+            :disabled="false"
+            href="/test3">
+            Item 4
+        </x-theme-list-item>
+
+    </x-theme-list-group>
+    <br />
+
+@endforeach
 <br/>
 <br/>
 <br/>
