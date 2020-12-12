00b83d2194ffb3c86f27563f0a26670df4ff9c26
Toby Twigger <tobytwigger1@gmail.com>
2020-12-12 10:48:17 +0000

    Start abstracting database user functionality

--- app/Providers/ControlServiceProvider.php
+++ app/Providers/ControlServiceProvider.php
@@ -1,11 +1,7 @@
 <?php
 
-
 namespace App\Providers;
 
-
-use BristolSU\ControlDB\Contracts\Repositories\DataUser;
-use BristolSU\ControlDB\Contracts\Repositories\Pivots\UserGroup;
 use BristolSU\ControlDB\ControlDBServiceProvider;
 
 class ControlServiceProvider extends ControlDBServiceProvider
