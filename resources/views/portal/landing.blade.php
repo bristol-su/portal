526801ae7dc7827418e4ae851c822bf0bc8f92f6
Aidan Laycock <aidanl_@hotmail.co.uk>
2021-08-30 19:55:34 +0100

    Landing Page Update

--- resources/views/portal/landing.blade.php
+++ resources/views/portal/landing.blade.php
@@ -4,19 +4,19 @@
 
 @section('app-content')
     <p-featured-card
-        title="Bristol SU Portal"
+        title="Welcome to the Portal"
         subtext="{{ \App\Settings\Appearance\Messaging\LandingPageTitle::getValue() }}"
         bg="landing-page"
     >
-        <p-button variant="white" href="{{route('login')}}">
+        <p-button class="w-full px-16 py-2 my-2 text-base font-medium text-blueGray-600 transition duration-500 ease-in-out transform rounded-md border-blueGray-50 bg-blueGray-50 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-100" href="{{route('login')}}">
             Login
         </p-button>
-        <p-button variant="black" href="{{route('register')}}">
-            Register
+        <p-button variant="secondary" class="w-full px-16 py-2 my-2 text-base font-medium text-white transition duration-500 ease-in-out transform border-black rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-900" href="{{route('register')}}">
+            Register an account
         </p-button>
     </p-featured-card>
 @endsection
 
-@push('scripts')
-    <script src="{{ mix('js/app.js') }}"></script>
-@endpush
+{{--@push('scripts')--}}
+{{--    <script src="{{ mix('js/app.js') }}"></script>--}}
+{{--@endpush--}}
