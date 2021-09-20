786d18e9df61540fea1480e5f30fd21795f25d0a
Toby Twigger <tobytwigger1@gmail.com>
2021-09-20 02:19:07 +0100

    Add error page

--- resources/views/errors/error.blade.php
+++ resources/views/errors/error.blade.php
@@ -3,21 +3,12 @@
 @section('title', 'An error occured')
 
 @section('app-content')
-    <p-page-content
-        class="text-center"
-        title="Whoops!"
-        subtitle="Something went wrong | Code {{method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : $exception->getCode()}}">
+    <p-error-page
+        message="{{($exception instanceof \Symfony\Component\HttpKernel\Exception\HttpException) ? $exception->getMessage() : ''}}"
+        code="{{method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : $exception->getCode()}}"
+        email="bristol-su@bristol.ac.uk"
+        home-url="{{route(request()->is(['a', 'a/*']) ? 'admin' : 'participant')}}"
+    >
 
-        @if($exception instanceof \Symfony\Component\HttpKernel\Exception\HttpException)
-            {{$exception->getMessage()}}
-        @endif
-
-        <hr class="border-0 bg-gray-500 text-gray-500 h-px my-3">
-
-        If this error persists, please contact us for help at <a href="mailto:bristol-su@bristol.ac.uk">bristol-su@bristol.ac.uk.</a>
-
-        <p-button type="secondary" href="{{route(request()->is(['a', 'a/*']) ? 'admin' : 'participant')}}">Take me
-            home
-        </p-button>
-    </p-page-content>
+    </p-error-page>
 @endsection
