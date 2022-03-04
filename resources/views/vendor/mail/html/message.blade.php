789ea69739e426ba16e77f06634f611dbfaf8632
Toby Twigger <tobytwigger1@gmail.com>
2022-03-04 12:10:27 +0000

    Update email footer

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -1,14 +1,26 @@
 @component('mail::layout')
 {{-- Header --}}
 @slot('header')
-@component('mail::header', ['url' => config('app.url')])
-[logo]: {{asset('/images/email_header.png')}} "Reaffiliation 2022/23"
+@component('mail::header', ['url' => config('app.url'), 'imageUrl' => asset('/images/email-header.png')])
+<img src="{{asset('/images/email-header.png')}}" alt="Reaffilation 2022/23">
 
 @endcomponent
 @endslot
 
 {{-- Body --}}
 {{ $slot }}
+
+<img src="{{asset('/images/logo.png')}}" style="max-width: 150px;" alt="Bristol SU Logo">
+
+<p style="font-size: 14px;">
+University of Bristol Students' Union,
+The Richmond Building, 105 Queens Road,
+Clifton, Bristol BS8 1LN
+</p>
+
+<p style="font-size: 14px;">
+Charity #1139656  Company #6977417
+</p>
 
 {{-- Subcopy --}}
 @isset($subcopy)
