bdaaf0d682d966149cd0c8c2e2dba630d70d2156
Toby Twigger <tobytwigger1@gmail.com>
2022-03-04 11:54:02 +0000

    Add email header

---
+++ resources/views/vendor/mail/html/message.blade.php
@@ -0,0 +1,28 @@
+@component('mail::layout')
+{{-- Header --}}
+@slot('header')
+@component('mail::header', ['url' => config('app.url')])
+[logo]: {{asset('/images/email_header.png')}} "Reaffiliation 2022/23"
+
+@endcomponent
+@endslot
+
+{{-- Body --}}
+{{ $slot }}
+
+{{-- Subcopy --}}
+@isset($subcopy)
+@slot('subcopy')
+@component('mail::subcopy')
+{{ $subcopy }}
+@endcomponent
+@endslot
+@endisset
+
+{{-- Footer --}}
+@slot('footer')
+@component('mail::footer')
+© {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+@endcomponent
+@endslot
+@endcomponent
