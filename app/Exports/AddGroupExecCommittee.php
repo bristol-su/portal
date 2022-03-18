e65e4e0982c16532b2099b901227af3862a6af12
Toby Twigger <tobytwigger1@gmail.com>
2022-03-18 17:07:05 +0000

    Filter by role year

--- app/Exports/AddGroupExecCommittee.php
+++ app/Exports/AddGroupExecCommittee.php
@@ -2,6 +2,7 @@
 
 namespace App\Exports;
 
+use BristolSU\ControlDB\Contracts\Models\Tags\RoleTag;
 use BristolSU\ControlDB\Export\FormattedItem;
 use BristolSU\ControlDB\Export\Formatter\Formatter;
 use BristolSU\ControlDB\Contracts\Models\Group;
@@ -14,15 +15,20 @@ class AddGroupExecCommittee extends Formatter
     {
         /** @var Group $group */
         $group = $formattedItem->original();
-        $formattedItem->addRow('President Email', $this->getEmailFromPositions($group, $this->config('president_positions')));
-        $formattedItem->addRow('Treasurer Email', $this->getEmailFromPositions($group, $this->config('treasurer_positions')));
-        $formattedItem->addRow('Secretary Email', $this->getEmailFromPositions($group, $this->config('secretary_positions')));
+        $formattedItem->addRow('President Email 2021', $this->getEmailFromPositions($group, $this->config('president_positions'), 'committee_year.y2021'));
+        $formattedItem->addRow('Treasurer Email 2021', $this->getEmailFromPositions($group, $this->config('treasurer_positions'), 'committee_year.y2021'));
+        $formattedItem->addRow('Secretary Email 2021', $this->getEmailFromPositions($group, $this->config('secretary_positions'), 'committee_year.y2021'));
+        $formattedItem->addRow('President Email 2022', $this->getEmailFromPositions($group, $this->config('president_positions'), 'committee_year.y2022'));
+        $formattedItem->addRow('Treasurer Email 2022', $this->getEmailFromPositions($group, $this->config('treasurer_positions'), 'committee_year.y2022'));
+        $formattedItem->addRow('Secretary Email 2022', $this->getEmailFromPositions($group, $this->config('secretary_positions'), 'committee_year.y2022'));
         return $formattedItem;
     }
 
-    private function getEmailFromPositions(Group $group, array $positionIds)
+    private function getEmailFromPositions(Group $group, array $positionIds, string $roleTagReference)
     {
-        $roles = $group->roles()->filter(fn(Role $role) => in_array($role->positionId(), $positionIds));
+        $roles = $group->roles()
+            ->filter(fn(Role $role) => in_array($role->positionId(), $positionIds))
+            ->filter(fn(Role $role) => $role->tags()->filter(fn(RoleTag $roleTag) => $roleTag->fullReference() === $roleTagReference)->count() > 0);
         return $roles->first()?->users()?->first()?->data()->email() ?? '';
     }
 
