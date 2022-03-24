550464e68db5f4fcf3886f260e17b4baccf00f28
Toby Twigger <tobytwigger1@gmail.com>
2022-03-24 10:32:28 +0000

    Add new db connection

--- config/database.php
+++ config/database.php
@@ -67,6 +67,26 @@ return [
             ]) : [],
 	],
 
+        'mysql_vapor_staging' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST_STAGING', '127.0.0.1'),
+            'port' => env('DB_PORT_STAGING', '3306'),
+            'database' => env('DB_DATABASE_STAGING', 'forge'),
+            'username' => env('DB_USERNAME_STAGING', 'forge'),
+            'password' => env('DB_PASSWORD_STAGING', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'url' => env('DATABASE_URL'),
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mysql_testing' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
