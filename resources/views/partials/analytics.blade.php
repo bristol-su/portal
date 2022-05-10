e44127bc86716e226f0f80c5214842865acd0add
Toby Twigger <tobytwigger1@gmail.com>
2022-05-10 13:42:17 +0100

    Add page speed and composer update

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -9,9 +9,22 @@
 
         gtag('js', new Date());
 
-        gtag('config', '{{config('app.analytics.UA')}}', {
-            'site_speed_sample_rate': 100
-        });
+        gtag('config', '{{config('app.analytics.UA')}}');
+
+        // Feature detects Navigation Timing API support.
+        if (window.performance) {
+            // Gets the number of milliseconds since page load
+            // (and rounds the result since the value must be an integer).
+            var timeSincePageLoad = Math.round(performance.now());
+
+            // Sends the timing event to Google Analytics.
+            gtag('event', 'timing_complete', {
+                'name': 'load',
+                'value': timeSincePageLoad,
+                'event_category': 'Page Load',
+                'event_label': window.location.href
+            });
+        }
     </script>
 @endif
 
